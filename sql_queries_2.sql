63789d29c0465624e0c6d033b431e55d676b5dc7
Emmanuel-Ryce-XGTS <152106768+Emmanuel-Ryce-XGTS@users.noreply.github.com>
2024-02-01 22:05:09 -0500

    Update sql_queries_2.sql

--- sql_queries_2.sql
+++ sql_queries_2.sql
@@ -1,1 +1,48 @@
+select count(*) from users where email like '%com';
 
+select first_name, COUNT(*) as 'COUNT' from users 
+group by first_name 
+order by 2 desc; 
+
+
+SELECT
+  ROUND(watch_duration_in_minutes,0) as duration,
+  COUNT(*) as count
+FROM watch_history
+GROUP BY duration
+ORDER BY duration ASC;
+
+
+SELECT user_id, SUM(amount) AS amounts
+FROM payments
+WHERE status = 'paid'
+GROUP BY user_id
+ORDER BY amounts DESC;
+
+
+select user_id,
+sum(watch_duration_in_minutes) as duration 
+from watch_history
+group by user_id having duration > 400;
+
+
+SELECT ROUND(SUM(watch_duration_in_minutes), 0) 
+ from watch_history
+
+
+SELECT pay_date AS day,
+   SUM(amount) AS total
+FROM payments
+WHERE status = 'paid'
+GROUP BY day
+ORDER BY total DESC;
+
+
+select avg(amount) from payments
+where status = 'paid'
+
+
+SELECT
+  MAX(watch_duration_in_minutes) AS max,
+  MIN(watch_duration_in_minutes) AS min
+FROM watch_history;
